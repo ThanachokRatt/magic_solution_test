2414c6939c60dedfdb1204fa1fa288f1dc12380c
Thanachok Rattanamora <126702871+ThanachokRatt@users.noreply.github.com>
2025-08-12 14:25:12 +0700

    feat:setting project

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppPaddings {
+  static const double small = 8.0;
+  static const double medium = 16.0;
+  static const double large = 24.0;
+  static const double superLarge = 48.0;
+}
+
+class AppFonts {
+  static const double small = 8.0;
+  static const double medium = 12.0;
+  static const double large = 16.0;
+  static const double superLarge = 28;
+}
+
+/// App Colors Const
+const Color kWhiteColor = Color(0xFFFFFFFF);
+const Color kPrimaryGray = Color(0xFF7F8C8D);
+const Color kLightGrayColor = Color(0xFFECECEC);
+const Color kSolidBlack = Color(0xFF000000);
+
+const Color kPrimaryBlue = Color(0xFF385CAD);
+const Color kBlueIosDialog = Color(0xFF007AFF);
